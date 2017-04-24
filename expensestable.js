d783c272912de8131b0c8472d8a9a2108149c682
Jay Shollenberger <jay@jayshollenberger.com>
2017-04-23 19:12:28 -0600

    Chart works initially, won't update..

--- expensestable.js
+++ expensestable.js
@@ -10,43 +10,103 @@ console.log(ediv);
 
 let expensedata = [];
 
-class DataFetcher {
+class ExpensesMaker {
+
     constructor() {
-        this.url = "http://localhost:5000";
+        this.url = "http://localhost:5000/expenses";
+        this.data = [];
+        this.chartmade = false;
     }
 
     getExpenses(startdate, enddate) {
-        this.expensesUrl = this.url + "/expenses";
+        let expensesurl = this.url;
         if (startdate !== null) {
-            this.expensesUrl += `?startdate=${startdate}`;
+            expensesurl += `?startdate=${startdate}`;
         }
         if (enddate !== null) {
-            this.expensesUrl += `&enddate=${enddate}`;
+            expensesurl += `&enddate=${enddate}`;
         }
-        this.expensesData = this._fetchData(this.expensesUrl);
+        this._fetchData(expensesurl);
     }
 
     _fetchData(url) {
-        let data = [];
         fetch(url)
             .then((response) => {
                 console.log(response.status);
                 return response.json();
             }).then((json) => {
-            data = this.generateexpensestable(json);
-            this.generatechart(data);
+            this.generateexpensestable(json);
+            if (this.chartmade) {
+                this.updateChart(json);
+            } else {
+                this.generatechart(json);
+                this.chartmade = true;
+            }
+            this.data = json;
 
         }).catch((error) => console.log(error));
-        return data;
     }
 
-    generatechart(data) {
-        var svg = d3.select("svg"),
-            width = +svg.attr("width"),
-            height = +svg.attr("height"),
-            radius = Math.min(width, height) / 2,
-            g = svg.append("g").attr("transform", "translate(" + width / 2 + ", " +
-                height / 2 + ")");
+
+    generatechart(dataset) {
+        let width = 500;
+        let height = 500;
+        let donutWidth = 75;
+        var radius = Math.min(width, height) / 2;
+        // var svg = d3.select("svg"),
+        //     width = +svg.attr("width"),
+        //     height = +svg.attr("height"),
+        //     radius = Math.min(width, height) / 2,
+        //     g = svg.append("g").attr("transform", "translate(" + width / 2 + ", " +
+        //         height / 2 + ")");
+
+        var svg = d3.select('#expenseschart')
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .append('g')
+            .attr('transform', 'translate(' + (width / 2) +
+                ',' + (height / 2) + ')');
+
+        var color = d3.scaleOrdinal(d3.schemeCategory20c);
+
+        var arc = d3.arc()
+            .outerRadius(radius)
+            .innerRadius(radius - donutWidth);
+
+        var pie = d3.pie()
+            .value(function (d) {
+                return +d.amount;
+            }).sort(null);
+
+        var path = svg.selectAll('path')
+            .data(pie(dataset))
+            .enter()
+            .append('path')
+            .attr('d', arc)
+            .attr('fill', function (d) {
+                return color(d.data.name);
+            });
+
+        this.pie = pie;
+        this.path = path;
+        this.arc = arc;
+    }
+
+    updateChart(data) {
+        this.path.data(this.pie(data));
+        this.path.transition()
+            .duration(750);
+            // .attrTween("d", this.arcTween);
+    }
+
+    arcTween(a) {
+        console.log(this);
+        var i = d3.interpolate(this._current, a);
+        this._current = i(0);
+        return (t) => {
+            return this.arc(i(t));
+        }
 
     }
 
@@ -83,11 +143,11 @@ function getFirstDateOfThisMonth() {
 function getExpensesByDate() {
     let startdate = document.querySelector('input[name=startdate]');
     let enddate = document.querySelector('input[name=enddate]');
-    let d = new DataFetcher();
-    d.getExpenses(startdate.value, enddate.value);
+    // let d = new ExpensesMaker();
+    df.getExpenses(startdate.value, enddate.value);
 }
 
 startdate.addEventListener('change', getExpensesByDate);
 enddate.addEventListener('change', getExpensesByDate);
-let df = new DataFetcher();
+let df = new ExpensesMaker();
 df.getExpenses(startdate.value, enddate.value);
